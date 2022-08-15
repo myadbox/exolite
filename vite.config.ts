e21b1ac8f0e96329a60208c3c82c2343842888f6
Joel Birch <joeldbirch@gmail.com>
2022-08-15 15:23:26 +1000

    Open browser on server start.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
     alias: {
