59f9a88567fdb53701daa1df1b212738d7a72209
Joel Birch <joeldbirch@gmail.com>
2022-09-06 15:49:56 +1000

    Exolite: postinstall script to rename package.

---
+++ dev/rename.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+// @ts-check
+
+// This script is used to change the name property of the package.json file to the parent directory name.
+
+import replace from 'replace'
+import packageJson from '../package.json'
+
+const exoliteName = `@myadbox/exolite`
+const name = packageJson.name
+
+if (name !== exoliteName) {
+  process.exit(0)
+}
+
+const newName = process.cwd().split('/').pop()
+
+replace({
+  regex: `"name": "${name}"`,
+  replacement: `"name": "${newName}"`,
+  paths: [`./package.json`],
+  recursive: false,
+  silent: false,
+})
