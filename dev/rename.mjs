1956f585045e8a5839e1cd6b9a4eb5b804293ddd
Joel Birch <joeldbirch@gmail.com>
2022-10-03 09:09:54 +1100

    DX: also change dev title.

--- dev/rename.mjs
+++ dev/rename.mjs
@@ -11,6 +11,7 @@ const require = createRequire(import.meta.url)
 const packageJson = require(`../package.json`)
 
 const exoliteName = `@myadbox/exolite`
+const exoliteTitle = `Sesimi template development`
 const name = packageJson.name
 
 if (name !== exoliteName) {
@@ -23,6 +24,14 @@ replace({
   regex: `"name": "${name}"`,
   replacement: `"name": "${newName}"`,
   paths: [`./package.json`],
+  recursive: false,
+  silent: false,
+})
+
+replace({
+  regex: `<title>${exoliteTitle}</title>`,
+  replacement: `<title>${newName}</title>`,
+  paths: [`./index.html`],
   recursive: false,
   silent: false,
 })
