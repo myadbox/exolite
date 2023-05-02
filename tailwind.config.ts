f2d56c21630bbfc4a14e41348f67437aeb504904
Joel Birch <joeldbirch@gmail.com>
2023-05-03 09:10:14 +1000

    Include toolkit in tailwind content.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import {Config} from 'tailwindcss'
 const config: Config = {
   content: [
     `./{src,dist}/**/*.{js,jsx,ts,tsx,svg}`,
-    // add any other files you want to scan for classes here, such as themes.
+    `./node_modules/@myadbox/exoplanet-toolkit/src/components/**/*.{js,jsx,ts,tsx,svg}`,
+    // add any other files you want to scan for classes here, such as themes and node_modules packages.
   ],
   theme: {
     extend: {},
